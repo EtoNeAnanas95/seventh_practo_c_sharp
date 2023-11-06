bd1135a98188e03aaa546c9145817076ac090b25
EtoNeAnanas95 <ananasovii.yashik@gmail.com>
2023-11-06 19:20:57 +0300

    first update

---
+++ ConsoleApp1/Function.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class function
+    {
+        public static void PrintHat(string name)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int width = Console.WindowWidth;
+
+            Console.SetCursorPosition((width / 2) - name.Length, 0);
+            Console.WriteLine(name);
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+        }
+
+        public static int ArrowMenuFirst(int length)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            int position = 2;
+            Console.SetCursorPosition(0, position);
+            Console.WriteLine("->");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey().Key;
+
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("  ");
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (position > 2) position--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (position <= length) position++;
+                        break;
+                    case ConsoleKey.Escape:
+                        return -1;
+                }
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+            }
+            while (key != ConsoleKey.Enter);
+            Console.Clear();
+            return position - 2;
+        }
+
+        public static int ArrowMenuMain(FileSystemInfo[] allItems)
+        {
+            int length = allItems.Length;
+            Console.ForegroundColor = ConsoleColor.White;
+            int position = 2;
+            Console.SetCursorPosition(0, position);
+            Console.WriteLine("->");
+            ConsoleKey key;
+            do
+            {
+                key = Console.ReadKey().Key;
+
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("  ");
+                switch (key)
+                {
+                    case ConsoleKey.UpArrow:ы
+                        if (position > 2) position--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (position <= length) position++;
+                        break;
+                    case ConsoleKey.Escape:
+                        return -1;
+                }
+                Console.SetCursorPosition(0, position);
+                Console.WriteLine("->");
+            }
+            while (key != ConsoleKey.Enter);
+
+            Open(allItems[position - 2]);
+            return 1;
+        }
+
+        public static void OpenFolder(DirectoryInfo mainDirectory)
+        {
+            int wait;
+            do
+            {
+                Console.Clear();
+                FileSystemInfo[] items = mainDirectory.GetFileSystemInfos();
+
+                PrintHat(mainDirectory.Name);
+
+                foreach (var item in items)
+                {
+                    if ((item.Attributes & FileAttributes.ReparsePoint) == 0)
+                    {
+                        Console.WriteLine($"  {item.Name}");
+                    }
+                }
+                wait = ArrowMenuMain(items);
+            }
+            while (wait != -1);
+        }
+
+        public static void OpenFile(FileInfo file)
+        {
+
+        }
+
+        public static void Open(FileSystemInfo item)
+        {
+            PrintHat(item.Name);
+
+            if (item is DirectoryInfo directory)
+            {
+                OpenFolder(directory);
+            }
+            else if (item is FileInfo file)
+            {
+                OpenFile(file);
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void PrintDrives()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            int width = Console.WindowWidth;
+            Console.SetCursorPosition((width / 2) - 7, 0);
+            Console.WriteLine("Этот компьютер");
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (DriveInfo driveInfo in allDrives)
+            {
+                Console.WriteLine($"  {driveInfo.Name} свободно: {driveInfo.TotalFreeSpace / 1073741824} GB из {driveInfo.TotalSize / 1073741824} GB");
+            }
+            Console.ResetColor();
+            DirectoryInfo mainDirectory = new DirectoryInfo(allDrives[function.ArrowMenuFirst(allDrives.Length)].Name);
+            function.Open(mainDirectory);
+        }
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            PrintDrives();
+            Console.Clear();
+        }
+    }
+}
